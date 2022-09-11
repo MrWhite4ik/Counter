a68a807a50c6f170d122b27fca7ada955be32e48
Nika <white4ik1995@gmail.com>
2022-09-11 15:40:46 +0400

    first commit

---
+++ main.js
@@ -0,0 +1,40 @@
+let increment = document.querySelector("#increment");
+let decrement = document.querySelector("#decrement");
+let reset = document.querySelector("#reset");
+let counterValue = document.querySelector("#counterValue");
+let button = document.getElementById("buttons")
+let counter = 0;
+
+const displayColor = () => {
+    //if number is negative it will be red
+    if (counter < 0) {
+      counterValue.id = "negative";
+    } //if number is positive it will be green
+    else if (counter > 0) {
+      counterValue.id = "positive";
+    }
+    //if number is zero it will be white
+    else {
+      counterValue.id = "display";
+    }
+  };
+
+
+increment.addEventListener("click" , ()=> {
+    counter++;
+    counterValue.innerText = counter;
+    displayColor();
+})
+;
+decrement.addEventListener("click" , ()=> {
+    counter--;
+    counterValue.innerText = counter;
+    displayColor();
+})
+reset.addEventListener("click" , ()=> {
+    counter = 0;
+    counterValue.innerText = counter;
+    displayColor();
+})
+
+
